969c0c1e11fc2e0b7d9000e092511d74b7432bcf
ShereneC <spyhopper@cableone.net>
2021-10-24 21:14:45 -0600

    init

---
+++ inspire-two.client/src/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const domain = 'sc-codeworks.us.auth0.com'
+export const audience = 'https://InspireTwo.com'
+export const clientId = 'yaSUL8hD2UjpLvalkKjXeLbXv2Jfs2II'
