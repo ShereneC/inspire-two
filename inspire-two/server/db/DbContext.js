15837faba56c23ab6fdebf6c68c65d3d4d67a812
ShereneC <spyhopper@cableone.net>
2021-10-26 11:24:59 -0600

    db tasks

--- inspire-two/server/db/DbContext.js
+++ inspire-two/server/db/DbContext.js
@@ -1,11 +1,13 @@
 import mongoose from 'mongoose'
 import { AccountSchema, ProfileSchema } from '../models/Account'
+import { TaskSchema } from '../models/Task'
 import { ValueSchema } from '../models/Value'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
   Profiles = mongoose.model('Profile', ProfileSchema, 'accounts');
+  Tasks = mongoose.model('Task', TaskSchema);
 }
 
 export const dbContext = new DbContext()
