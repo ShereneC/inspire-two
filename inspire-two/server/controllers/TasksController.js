53af7394351771a5f3340bf0fd0a54e179e8a771
ShereneC <spyhopper@cableone.net>
2021-10-27 11:17:24 -0600

    Task FindbyId

--- inspire-two/server/controllers/TasksController.js
+++ inspire-two/server/controllers/TasksController.js
@@ -1,22 +1,32 @@
 import BaseController from '../utils/BaseController'
 import { Auth0Provider } from '@bcwdev/auth0provider'
+import { tasksService } from '../services/TasksService'
 
 export class TasksController extends BaseController {
   constructor() {
     super('api/tasks')
     this.router
+      .get('', this.getAllTasks)
     // NOTE: Beyond this point all routes require Authorization tokens (the user must be logged in)
       .use(Auth0Provider.getAuthorizedUserInfo)
-      .get('', this.getAllTasks)
       .get('/:id', this.getTaskById)
       .post('', this.createTask)
-      .delete('', this.removeTask)
+      // .delete('', this.removeTask)
   }
 
   async getAllTasks(req, res, next) {
     try {
       const tasks = await tasksService.getAllTasks(req.query)
       res.send(tasks)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getTaskById(req, res, next) {
+    try {
+      const task = await tasksService.getTaskById(req.params.id)
+      res.send(task)
     } catch (error) {
       next(error)
     }
--- inspire-two/server/services/TasksService.js
+++ inspire-two/server/services/TasksService.js
@@ -3,12 +3,19 @@ import { BadRequest } from '../utils/Errors'
 
 class TasksService {
   async getAllTasks(query = {}) {
-    const tasks = await dbContext.Tasks.find(query)
+    const tasks = await dbContext.Tasks.find(query).populate()
     if (!tasks) {
       throw new BadRequest('Tasks not found')
     }
     return tasks
   }
-}
 
+  async getTaskById(id) {
+    const foundTask = await dbContext.Tasks.findById(id).populate()
+    if (!foundTask) {
+      throw new BadRequest('Invalid Task Id')
+    }
+    return foundTask
+  }
+}
 export const tasksService = new TasksService()
